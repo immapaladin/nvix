015770c9bccc69905f6579cbeb464d648a02cbe5
immapaladin <ericbrincheskie@gmail.com>
2024-06-30 22:03:22 -0500

    re-format neorg.nix

--- config/neorg.nix
+++ config/neorg.nix
@@ -1,16 +1,1 @@
-{
-  plugins.neorg.enable = true;
-
-  plugins.neorg.moduels = {
-    "core.defaults" = { __empty = null; };
-    "core.dirman" = {
-      config = {
-        workspaces = {
-          home = "~/notes/home";
-          work = "~/notes/work";
-        };
-      };
-    };
-
-  };
-}
+{ plugins.neorg = { enable = true; }; }
